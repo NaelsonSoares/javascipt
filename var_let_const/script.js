c57af4b9b3e519acb0c0bfb9fef51bbd202d5a92
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2023-01-17 22:15:53 -0300

    Variáveis e tipos.

---
+++ var_let_const/script.js
@@ -0,0 +1,30 @@
+var nome1 = 'João'
+var nome2 = 'Souza'
+
+if ( nome1 === 'João' ){
+    var nome1 = 'Pedro'
+    let nome2 = 'Silva'
+
+    console.log( nome1, nome2)
+}
+
+console.log( nome1, nome2)
+
+const NOME_PROPRIO = 'Neneca'
+
+console.log( NOME_PROPRIO)
+
+let nome = 'João'
+let sobrenome = 'Batista'
+ console.log( nome + sobrenome)
+ let concatenado = nome + sobrenome
+  console.log( concatenado.length )
+  console.log( concatenado [6] )
+  console.log( `${ nome } ${ sobrenome }` )
+  console.log( `${nome}
+  ${sobrenome}` )
+
+  let frase = 'Usando o, split'
+  console.log( frase.split( "" ) )
+  console.log( frase.split( " " ) )
+  console.log( frase ) // coloque um ponto após a variável e aparecerar as opções de metodos
